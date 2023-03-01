c34d787115b125c129cfd34ae8fdd5189d4e815a
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-01 20:28:50 +0100

    visualizzazione a schermo dell'elenco dei progetti, funzionante

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,4 +1,34 @@
 @extends('layouts.admin');
 @section('content')
-    Lista dei post
+    <div class="container">
+        <div class="row d-flex flex-wrap ">
+            <h1 class="col-12 my-5 text-info">
+                Elenco dei progetti: 
+            </h1>
+            @foreach ( $projects as $project )
+            <div class="card border-info m-3 p-0" style="max-width: 250px">
+                <div class="card-header">Titolo: {{$project->title}}</div>
+                <div class="card-body text-info">
+                  <h5 class="card-title text-dark">Slug: {{$project->slug}}</h5>
+                  <p class="card-text">{{$project->content}}</p>
+                </div>
+                <div class="text-center">
+                    <a href="" class="btn btn-sm btn-square btn-warning">
+                        <i class="fa-solid fa-pen"></i>
+                    </a>
+                    <a href="" class="btn btn-sm btn-square btn-success">
+                        <i class="fa-solid fa-plus"></i>
+                    </a>
+                    <form action="" method="POST" class="d-inline-block">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-danger btn-square">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
