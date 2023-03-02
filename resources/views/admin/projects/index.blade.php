3251948ff5fa1d54bca7e6ddf13c5212fa9f047b
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-02 20:42:44 +0100

    collegamento rotta show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,35 +1,35 @@
-@extends('layouts.admin');
+@extends('layouts.admin')
 @section('content')
     <div class="container">
         <div class="row d-flex flex-wrap ">
             <h1 class="col-12 my-5 text-info">
                 Elenco dei progetti: 
             </h1>
-            @foreach ( $projects as $project )
-          
-                <div class="cta m-2">
-                   <img src="{{$project->image}}" alt="">
-                   <div class="text">
-                      <h2>{{$project->title}}</h2>
-                      <p>{{$project->content}}</p>
-                      <div class="text-center">
-                           <a href="" class="btn btn-md btn-square btn-warning">
-                               <i class="fa-solid fa-pen"></i>
-                           </a>
-                           <a href="" class="btn btn-md btn-square btn-success">
-                               <i class="fa-solid fa-plus"></i>
-                           </a>
-                           <form action="" method="POST" class="d-inline-block">
-                               @csrf
-                               @method('DELETE')
-                               <button type="submit" class="btn btn-md btn-danger btn-square">
-                                   <i class="fa-solid fa-trash"></i>
-                               </button>
-                           </form>
+        @foreach ( $projects as $project )
+        <div class="cta m-2 ">
+                        <a href="{{route('admin.projects.show',  ['project' => $project['id']])}}" class="col-12">
+                       <img src="{{$project->image}}" alt="">
+                       <div class="text">
+                          <h2>{{$project->title}}</h2>
+                          <p>{{$project->content}}</p>
+                          <div class="text-center">
+                               <a href="" class="btn btn-md btn-square btn-warning">
+                                   <i class="fa-solid fa-pen"></i>
+                               </a>
+                               <a href="" class="btn btn-md btn-square btn-success">
+                                   <i class="fa-solid fa-plus"></i>
+                               </a>
+                               <form action="" method="POST" class="d-inline-block">
+                                   @csrf
+                                   @method('DELETE')
+                                   <button type="submit" class="btn btn-md btn-danger btn-square">
+                                       <i class="fa-solid fa-trash"></i>
+                                   </button>
+                               </form>
+                           </div>
                        </div>
-                   </div>
-                </div>
-                
+                    </a>
+                    </div>
             @endforeach
         </div>
     </div>
