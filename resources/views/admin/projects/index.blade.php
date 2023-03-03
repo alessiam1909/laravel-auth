1f4e96d5620837a63dd9ba31c09e2703eaea3b96
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-03 09:55:22 +0100

    aggiunta pagina show e collegamenti rotte con vari pulsanti

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,19 +7,19 @@
             </h1>
         @foreach ( $projects as $project )
         <div class="cta m-2 ">
-                        <a href="{{route('admin.projects.show',  ['project' => $project['id']])}}" class="col-12">
+                        <a href="{{route('admin.projects.show',  ['project' => $project['slug']])}}" class="col-12">
                        <img src="{{$project->image}}" alt="">
                        <div class="text">
                           <h2>{{$project->title}}</h2>
                           <p>{{$project->content}}</p>
                           <div class="text-center">
-                               <a href="" class="btn btn-md btn-square btn-warning">
+                               <a href="{{route('admin.projects.edit', ['project' =>$project->slug])}}" class="btn btn-md btn-square btn-warning">
                                    <i class="fa-solid fa-pen"></i>
                                </a>
-                               <a href="" class="btn btn-md btn-square btn-success">
+                               <a href="{{route('admin.projects.create')}}" class="btn btn-md btn-square btn-success">
                                    <i class="fa-solid fa-plus"></i>
                                </a>
-                               <form action="" method="POST" class="d-inline-block">
+                               <form action="{{route('admin.projects.destroy',  $project->slug)}}" method="POST" class="d-inline-block">
                                    @csrf
                                    @method('DELETE')
                                    <button type="submit" class="btn btn-md btn-danger btn-square">
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,7 +53,7 @@
                         </ul>
                     </div>
                 </nav>
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <main class="col-md-9 col-lg-10 ">
                     @yield('content')
                 </main>
             </div>
