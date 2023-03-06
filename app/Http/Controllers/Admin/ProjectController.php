acd1ba52242ebc1971309907d6536d3bfa048929
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-06 10:06:14 +0100

    aggiunta funzione edit nel controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -64,7 +64,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
